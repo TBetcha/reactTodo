aa89edce98735f2bd564339fdb34bed5edbb0d39
Tbetcha <troy.boettger@gmail.com>
2020-06-05 00:28:04 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+/** @format */
+
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+
+class App extends Component {
+	state = {
+		todos: [
+			{
+				id: 1,
+				title: 'Take out the trash',
+				completed: false,
+			},
+			{
+				id: 2,
+				title: 'Study React',
+				completed: true,
+			},
+			{
+				id: 3,
+				title: 'Eat Dinner',
+				completed: false,
+			},
+		],
+	};
+
+	markComplete = (id) => {
+		this.setState({
+			todos: this.state.todos.map((todo) => {
+				if (todo.id === id) {
+					todo.completed = !todo.completed;
+				}
+				return todo;
+			}),
+		});
+	};
+
+	render() {
+		console.log(this.state.todos);
+		return (
+			<div className="App">
+				<Todos todos={this.state.todos} markComplete={this.markComplete} />
+			</div>
+		);
+	}
+}
+export default App;
---
+++ src/components/Todoitem.js
@@ -0,0 +1,37 @@
+/** @format */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Todos from './Todos';
+
+export class Todoitem extends Component {
+	getStyle = () => {
+		return {
+			background: 'f4f4f4',
+			padding: '10px',
+			borderBottom: '1px #ccc dotted',
+			textDecoration: this.props.todo.completed ? 'line-through' : 'none',
+		};
+	};
+
+	render() {
+		const { id, title } = this.props.todo;
+		return (
+			<div style={this.getStyle()}>
+				<p>
+					<input type="checkbox" onChange={this.props.markComplete.bind(this, id)} />
+					{title}
+				</p>
+			</div>
+		);
+	}
+}
+Todoitem.propTypes = {
+	todo: PropTypes.object.isRequired,
+};
+
+const itemStyle = {
+	backgroundColor: '#f4f4f4',
+};
+
+export default Todoitem;
